f6f43e0777125ff7ec91461ecfe8fcd65fc2d3aa
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-10 19:11:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
   my_array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  my_array.first
+  my_array.[0]
 end
 
 def retrieve_last_element_from_array(array)
